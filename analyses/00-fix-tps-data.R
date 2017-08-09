5c9e3118c126ebd0dc483169509b452bec41f068
Natalie Cooper <ncooper@tcd.ie>
2017-08-09 16:37:01 +0100

    Minor edits to comments

--- analyses/00-fix-tps-data.R
+++ analyses/00-fix-tps-data.R
@@ -28,7 +28,7 @@ slands <- readland.tps(file = "rawdata/dolphin-crania-unscaled.TPS",
 # This was an error made in landmarking but has an easy fix
 skull <- slands[-c(13, 17, 18, 22, 23, 25, 26, 28), , ]
 
-# Some of the pictures have the wrong names
+# Some of the image IDs are truncated
 # Change these so they match the other data files
 dimnames(skull)[[3]] <- gsub("ventralf", "ventral.tiff", dimnames(skull)[[3]])
 
@@ -49,9 +49,7 @@ mandible <- mlands[-c(9, 11, 12, 14), , ]
 
 # Fix issues with names missing tiff. 
 dimnames(mandible)[[3]]  <- gsub("dorsalf", "dorsal.tiff", dimnames(mandible)[[3]])
-#dimnames(mandible)[[3]] <- gsub("Inia_geoffrensis ", "Inia_geoffrensis",  dimnames(mandible)[[3]])
-#dimnames(mandible)[[3]]  <- gsub("Grampus_griseus", "Grampus_griseus", dimnames(mandible)[[3]])
-#dimnames(mandible)[[3]]  <- gsub("Inia_geoffrensis", "Inia_geoffrensis", dimnames(mandible)[[3]])
 
 # Write out new TPS file
 writeland.tps(mandible, "data/dolphin-mandibles.TPS")
+
